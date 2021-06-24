2a1821f876168e4104d280158261c80c1b565dfe
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-06-24 18:46:00 +0200

    Class Restructured to include extraction of information for a single data point. function for finding nearest coordinate also implemented.

--- src/ERA5.py
+++ src/ERA5.py
@@ -69,6 +69,62 @@ class ERA5():
                        'longitude' : lon_file[idx_lon], 'latitude' : lat_file[idx_lat]}
         return output_dict
     
+    def check_availability(self):
+        #function to evaluate percentage of the times the data point has 
+        #availability of data
+        pass
+    
+    def load_coordinate_data(self, lon_user, lat_user, variable, output_directory, file_type = 'Joint'):
+        #Function to extract information about a single point
+        
+        #input
+        #lon_user : float64 : longitudnal value for which information is required
+        #lat_user : float64 : latitudnal value for which information is required
+        #variable : string : specify exactly the property to study. by default, 
+        #all variables can be obtained but for now, we will limit the functionality to include only one variable specified by the user
+        #output_directory : string : Provide directory to save the data file
+        #file_type : string : 
+        #if exact grid points for which data is available not entered, function 
+        #finds the nearest data point. 
+        nearest_grid_point = self.near_point(self.lon, self.lat, lon_user, lat_user)
+        
+        if not lon_user in self.lon and lat_user in self.lat:
+            print ('The entered longitudnal coordinate does not have an associated value, value from the closest data point will be used.')
+            print('The nearest point to your query is: Longitude: ' + str(nearest_grid_point['longitude']) + ', Latitude - ' + str(nearest_grid_point['latitude']))
+        
+        #if else structure to support code in the for loop for joint data frame output
+        if file_type == 'Joint':
+            joint_df = pd.DataFrame()
+            
+        for file in self.files:
+            f = Dataset(self.directory + file, 'r')
+            #unpack all variables of interest
+            time = f.variables['time']
+            dtime = num2date(time[:], time.units) #hours since 1900-1-1 00:00:00"
+            #unpack variable of interest into var
+            var = f.variables[variable][:]
+            #slice the var to extract the data for the data point only
+            var_point = var[:, nearest_grid_point['lat index'], nearest_grid_point['lon index']]
+            
+            #create dataframe with the var information along with the lon/lat and date
+            df = pd.DataFrame({'Date' : np.array(dtime[:]), 'Longitude' : nearest_grid_point['longitude'], \
+                               'Latitude' : nearest_grid_point['latitude'],\
+                                   variable : var_point})
+                
+            #write the output to csv files
+            if file_type == 'Separate':
+                df.to_csv(output_directory + file[:-3] + variable + '.csv')
+            
+                #concat the dataframe to produce one file
+            else:
+                joint_df = pd.concat([joint_df, df], ignore_index=True)
+                
+        if file_type == 'Joint':
+            joint_df.to_csv(output_directory + 'Joint ' + variable + '.csv')
+                
+   
+    
+   
     def load_bounded_region(self, output_directory, file_type = 'Joint'):
 
         #this function will load the data files and extract the values for the bounded region
@@ -81,7 +137,9 @@ class ERA5():
 
         #if else structure to support code in the for loop for joint data frame output
         if file_type == 'Joint':
-            joint_df = pd.DataFrame()
+            joint_df_swh = pd.DataFrame()
+            joint_df_mwp = pd.DataFrame()
+            joint_df_mwd = pd.DataFrame()
             
         for file in self.files:
             f = Dataset(self.directory + self.file, 'r') #common mistake is to not provide full path of the dataset
@@ -95,8 +153,8 @@ class ERA5():
             #extract the bounded region
             #+1 is incldued in the slicing because the end point is not included. 
             #by adding 1 to the end point, the last coordinate data is also included
-            lon_reg = self.lon[self.vertex1['lon index']:self.vertex2['lon index']+1]
-            lat_reg = self.lat[self.vertex1['lat index']:self.vertex2['lat index']+1]
+            lon_reg = self.lon[self.vertex1['lon index']: self.vertex2['lon index']+1]
+            lat_reg = self.lat[self.vertex1['lat index']: self.vertex2['lat index']+1]
             swh_reg = swh[:, self.vertex1['lat index'] : self.vertex2['lat index'] + 1, self.vertex1['lon index'] : self.vertex2['lon index']+1]
             mwd_reg = mwd[:, self.vertex1['lat index'] : self.vertex2['lat index'] + 1, self.vertex1['lon index'] : self.vertex2['lon index']+1]
             mwp_reg = mwp[:, self.vertex1['lat index'] : self.vertex2['lat index'] + 1, self.vertex1['lon index'] : self.vertex2['lon index']+1]
@@ -125,33 +183,47 @@ class ERA5():
         
             #declare a dataframe with the long and lat repeated for the entire length
                 #of the dtime.
-            df = pd.DataFrame(columns = columns)
-            df = pd.DataFrame({'Date' : None, 'longitude' : np.tile(LON, len(dtime)), \
+            df_swh = pd.DataFrame(columns = columns)
+            df_swh = pd.DataFrame({'Date' : None, 'longitude' : np.tile(LON, len(dtime)), \
                                    'latitude' : np.tile(LAT, len(dtime))})
             
-                
+           
+            df_mwp = pd.DataFrame(columns = columns)
+            df_mwp = pd.DataFrame({'Date' : None, 'longitude' : np.tile(LON, len(dtime)), \
+                                   'latitude' : np.tile(LAT, len(dtime))})
+           
+            df_mwd = pd.DataFrame(columns = columns)
+            df_mwd = pd.DataFrame({'Date' : None, 'longitude' : np.tile(LON, len(dtime)), \
+                                   'latitude' : np.tile(LAT, len(dtime))})
+           
                 #declare a dtime dataframe with repeated values for the length of the long and lat
             #dt = (pd.DataFrame({'Date' : dtime[:]}))
 
             #following code is to reproduce
             x = np.array(dtime[:])
             x = np.repeat(x, len(lon_reg)*len(lat_reg))
-            df['Date'] = x
-            df['swh'] = swh_arr
-            df['mwd'] = mwd_arr
-            df['mwp'] = mwp_arr
+            df_swh['Date'] = x; df_mwd['Date'] = x; df_mwp['Date'] = x; 
+            df_swh['swh'] = swh_arr
+            df_mwd['mwd'] = mwd_arr
+            df_mwp['mwp'] = mwp_arr
                 
             
             if file_type == 'Separate':
-                df.to_csv(output_directory + file[:-3] +'.csv')
+                df_swh.to_csv(output_directory + file[:-3] +'_swh.csv')
+                df_mwp.to_csv(output_directory + file[:-3] +'_mwp.csv')
+                df_mwd.to_csv(output_directory + file[:-3] +'_mwd.csv')
 
             elif file_type == 'Joint':
-                joint_df = pd.concat([joint_df, df], ignore_index=True)
+                joint_df_swh = pd.concat([joint_df_swh, df_swh], ignore_index=True)
+                joint_df_mwp = pd.concat([joint_df_mwp, df_mwp], ignore_index=True)
+                joint_df_mwd = pd.concat([joint_df_mwd, df_mwd], ignore_index=True)
                 
         if file_type == 'Joint':
-            joint_df.to_csv(output_directory + 'Joint.csv')
+            joint_df_swh.to_csv(output_directory + 'Joint_swh.csv')
+            joint_df_mwd.to_csv(output_directory + 'Joint_mwd.csv')
+            joint_df_mwp.to_csv(output_directory + 'Joint_mwp.csv')
             
-            
+    
 '''        
         
 
