3c816e79667621e33ca6985d0ccf0777126302cd
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-07-23 11:43:32 +0200

    sums accumulatives added, rows flipped, color schemes adjusted.

--- src/freq_occurence.py
+++ src/freq_occurence.py
@@ -26,18 +26,20 @@ from matplotlib.ticker import MaxNLocator, MultipleLocator
 import cartopy as ct
 from matplotlib.font_manager import FontProperties
 import seaborn as sns
+from datetime import datetime
 
 
 direc = 'tests/results/'
 df = pd.read_csv(direc + '07_09-05_10_PM.csv', index_col = False)
 df['Date'] = pd.to_datetime(df['Date'])
 
-y = df['hmax (m)'].to_numpy()
+y = df['swh (m)'].to_numpy()
 x = df['mwp (s)'].to_numpy()
-bins_y = np.linspace(np.floor(min(y)), np.ceil(max(y)), int(np.ceil(max(y)) - np.floor(min(y)))*1+1) #converts height into 0.5 bins
+bins_y = np.linspace(np.floor(min(y)), np.ceil(max(y)), int(np.ceil(max(y)) - np.floor(min(y)))*2+1) #converts height into 0.5 bins
 bins_x = np.linspace(np.floor(min(x)), np.ceil(max(x)), int(np.ceil(max(x)) - np.floor(min(x)))+1) #returns bins with detla=1 bins
 counts, xedges, yedges = np.histogram2d(x, y, bins=(bins_x, bins_y))
 counts = counts/len(df) * 100
+counts = np.flip(counts, axis = 0)
 #following loops are important to create customized labels. 
 #otherwise, instead of range, single value points are specified. 
 xnew = [] 
@@ -48,7 +50,8 @@ for i in range(len(xedges)):
         d = str(xedges[i]) + ' - ' + str(xedges[i+1])  
         xnew = np.append(xnew, d)
 #add a total 
-xnew = np.append(xnew, ['Sum']) #add additional column for the sum and the cumulative sum
+xnew = np.flip(xnew, axis = 0)
+xnew = np.append(xnew, ['Sum', 'Accumulative']) #add additional column for the sum and the cumulative sum
 
 ynew = []
 for i in range(len(yedges)):
@@ -58,55 +61,69 @@ for i in range(len(yedges)):
         d = str(yedges[i]) + ' - ' + str(yedges[i+1])  
         ynew = np.append(ynew, d)
 
-ynew = np.append(ynew, ['Sum']) #add additional column for the sum and additonal column
+ynew = np.append(ynew, ['Sum', 'Accumulative']) #add additional column for the sum and additonal column
 
 sum_col_arr = np.sum(counts, axis = 0).reshape(1,-1)
-counts = np.append(counts, sum_col_arr, axis = 0)
+acc_col = [] #accumulative of each column
+for i in range(1,np.shape(sum_col_arr)[1]+1):
+    acc_col = np.append(acc_col, np.sum(sum_col_arr[0, 0:i])).reshape(1, -1)
+
+sum_col = np.append(sum_col_arr, acc_col, axis = 0) #merge the two columns together for the sums
+#sum_col = np.round(sum_col, 3)
+counts = np.append(counts, sum_col, axis = 0)
 
 
 sum_row_arr = np.sum(counts, axis = 1, keepdims = True)
-counts = np.append(counts, sum_row_arr, axis = 1)
+acc_row = [] #accumulative of each row
+for i in range(1,np.shape(sum_row_arr)[0]+1):
+    acc_row = np.append(acc_row, np.sum(sum_row_arr[:i, 0])).reshape(-1, 1)
+sum_row = np.append(sum_row_arr, acc_row, axis = 1)
+#sum_row = np.round(sum_row, 3)
+counts = np.append(counts, sum_row, axis = 1)
 
-#function to generate a matrix of accumulative totals from the individual sums
-def accumulative_(sum_arr):
-    total = []
-    for i in range(1, len(sum_arr)+1):
-        dummy = np.sum(sum_arr[:i])
-        total = np.append(total, dummy)
-    return total
-        
-total_row = accumulative_(sum_row_arr)
-total_col = accumulative_(sum_col_arr.T)
-df2 = pd.DataFrame(counts)
+counts[np.shape(counts)[0]-1, np.shape(counts)[1]-1] = 0
+counts[np.shape(counts)[0]-1, np.shape(counts)[1]-1-1] = 0
+counts[np.shape(counts)[0]-1-1, np.shape(counts)[1]-1] = 0
+
+
+
 mask = counts == 0 #exclue all 0s. cant change colour
 #change the last row and the last column to True and mask it
 #this will exclude them from the plotting of heatmap
 #the main heatmap will still be of the actual content within the counts array
-mask[13, :] = True
-mask[:, 22] = True
+mask[-2:, :] = True
+mask[:, -2:] = True
 
 
 plt.figure(figsize = (30,30))
 #x and y labels are reversed to change orientation of the curve
+
+#mask all elements except the last row and last column
+mask2 = counts == 0 #exclue all 0s. cant change colour
+mask2[:,:] = True
+mask2[-2,:] = False
+mask2[:,-2] = False
+mask2[np.shape(counts)[0]-1, np.shape(counts)[1]-1] = True#remove the 0s at the end of the two rows and columns
+mask2[np.shape(counts)[0]-1, np.shape(counts)[1]-1-1] = True
+mask2[np.shape(counts)[0]-1-1, np.shape(counts)[1]-1] = True
+
+mask3 = counts == 0 #exclue all 0s. cant change colour
+mask3[:,:] = True
+mask3[-1:,:] = False
+mask3[:,-1:] = False
+mask3[np.shape(counts)[0]-1, np.shape(counts)[1]-1] = True#remove the 0s at the end of the two rows and columns
+mask3[np.shape(counts)[0]-1, np.shape(counts)[1]-1-1] = True
+mask3[np.shape(counts)[0]-1-1, np.shape(counts)[1]-1] = True
+
 ax = sns.heatmap(counts, annot = True, linewidths=0.5, mask = mask,\
                  cbar = False, xticklabels=ynew, yticklabels=xnew, annot_kws={"size": 'small'})
     
-
-
-mask2 = counts == 0 #exclue all 0s. cant change colour
-#mask all elements except the last row and last column
-mask2[:,:] = True
-mask2[-1,:] = False
-mask2[:,-1] = False
 ax2 = sns.heatmap(counts, mask = mask2, annot = True, linewidths=0.5,\
-                 cbar = False, cmap='RdBu', xticklabels=ynew, yticklabels=xnew, annot_kws={"size": 'small', 'color':'white'})
+                 cbar = False, xticklabels=ynew, yticklabels=xnew, annot_kws={"size": 'small'}, fmt = 'f')
 
-plt.tick_params('both', bottom=False, left=False)
-plt.ylabel('mwp (s)')
-plt.xlabel('hmax (s)')
-plt.title('Frequency of Occurance (mwp vs $H_{max}$)')
-
-plt.tight_layout()
+ax3 = sns.heatmap(counts, mask = mask3, annot = True, linewidths=0.5,\
+                 cbar = False, xticklabels=ynew, yticklabels=xnew, annot_kws={"size": 'small'}, fmt = 'f')
+    
 z = []
 for t in ax.texts: 
     
@@ -116,6 +133,24 @@ for t in ax.texts:
         t.set_text('{0:.0E}'.format(a))
     if (a > 0.01):
         t.set_text('{:.2f}'.format(a))
+    if (a > 99):
+        t.set_text('{:.3f}'.format(a))
+    if (a == 0):
+        t.set_text('{:.0f}'.format(a))
 
 
+
+
+plt.tick_params('both', bottom=False, left=False)
+plt.ylabel('mwp (s)')
+plt.xlabel('swh (m)')
+plt.title('Frequency of Occurance (mwp vs $swh$)')
+
+plt.tight_layout()
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+
+plt.savefig('Plots/'+current_time + '.pdf')
 
