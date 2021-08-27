e3efe164cb5416a92406dd051d66d7aadee94783
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-08-27 22:55:50 +0200

    Table formed for the 12 months and yearly.

--- src/weather_window.py
+++ src/weather_window.py
@@ -17,6 +17,8 @@ remove the bearing and magnitude calculation from this file when automating.
 dump wind calculation functions into another file
 
 when automating the script, run the entire code from filteration of monthly values to the end in a loop 12 times
+
+Implement a detla time calculation to confirm hourly data
 """
 
 
@@ -24,7 +26,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-
+from matplotlib.font_manager import FontProperties
 
 def bearing(vector):
 #function to calculate the bearing. takes in the resultant vector
@@ -53,63 +55,85 @@ df['magnitude'] = np.sqrt(df['u10']**2 + df['v10']**2)
 resultant_vector = np.array(df[{'u10', 'v10'}].values.tolist())
 angle = bearing(resultant_vector)    
 df['angle'] = angle
-
-
-Jan = df[df['Start Date'].dt.month==1].reset_index(drop=True)
 swh_limit = 1.5
 vel_limit = 5
 
-
-#codntion should be with or condition. otherwise, nan comes because both 
-#conditions ahve to be met for a False which isnt always the case. when any
-# of the two conditions become invalid, a false should be registered. 
-#For true however, both conditions should be met
-Jan.loc[(Jan['swh'] <= swh_limit) & (Jan['magnitude'] <= vel_limit), 'bool'] = True
-Jan.loc[(Jan['swh'] > swh_limit) | (Jan['magnitude'] > vel_limit), 'bool'] = False 
-
-
-v = np.array(Jan['bool'])
-
-#two methods for identifying when the boolean changes from true to false and vice versa
-#this method is deprecated and hence error prone. however, for future reference, 
-#can be interesting
-#x = np.where(v[:-1] != (v[1:]))
-
-#gives us the index everytime boolean changes
-#one has to be added to i and subtracted from i+1
-y = np.where(np.diff(v))[0]
-new_df = Jan.loc[y] #queries the original dataframe to extract only relevent indices where the conditions are met
-#create another column with the end date
-#first reset the indices 
-new_df = new_df.reset_index(drop = True)
-
-new_df.insert(1, 'End Date', None)#specifies location for adding the end date column
-new_df.loc[0:(len(new_df)-2), 'End Date'] = new_df['Start Date'][1:].reset_index(drop = True)
-new_df['End Date'] = pd.to_datetime(new_df["End Date"])
-#calculate the difference between each element 
-#these are hourly calculations
-new_df.insert(2, 'Duration', (new_df['End Date'] - new_df['Start Date']).astype('timedelta64[h]'))
-
-#when binning monthly, there is a cell when the year changes. 
-#this means that the duration will always be around a year's difference
-#we first identify where the year changes, then make that cell none
-#np.where identifies where the year changes
-#with loc, we alter the cell value
-new_df.loc[np.where((new_df['End Date'].dt.year - new_df['Start Date'].dt.year) >= 1)[0], 'Duration'] = None
-
-
-
+#13th month is not month
+#it is added to provide iteration for the year
+months = np.linspace(1,13,13).astype(int) #for data filtering using datetime
 
 #dataframe with the compiled results which will be published
 columns = ['>= Period (h)', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 
-                                  'Oct', 'Nov', 'Dec']
-results = pd.DataFrame(columns = columns)
+                                  'Oct', 'Nov', 'Dec', 'Year']
+results = pd.DataFrame(columns = columns, dtype = float)
 results['>= Period (h)'] = [3, 6, 12, 24, 48, 72]
 
-for i in results['>= Period (h)']:
-    x = (new_df[new_df['Duration'] >= i])['Duration'].sum(axis = 0)/len(new_df)
-    results.loc[results['>= Period (h)'] == i, 'Jan'] = x
-#calculate the percentages for when the values are greater than each 
-#of the durations specified by the period column of results
+for m, col in zip(months, columns[1:]):
+    if m == 13:
+        month_df = df
+    else:    
+        month_df = df[df['Start Date'].dt.month==m].reset_index(drop=True)
+    
+    
+    
+    #codntion should be with or condition. otherwise, nan comes because both 
+    #conditions ahve to be met for a False which isnt always the case. when any
+    # of the two conditions become invalid, a false should be registered. 
+    #For true however, both conditions should be met
+    month_df.loc[(month_df['swh'] <= swh_limit) & (month_df['magnitude'] <= vel_limit), 'bool'] = True
+    month_df.loc[(month_df['swh'] > swh_limit) | (month_df['magnitude'] > vel_limit), 'bool'] = False 
+    
+    
+    v = np.array(month_df['bool'])
+    
+    #two methods for identifying when the boolean changes from true to false and vice versa
+    #this method is deprecated and hence error prone. however, for future reference, 
+    #can be interesting
+    #x = np.where(v[:-1] != (v[1:]))
+    
+    #gives us the index everytime boolean changes
+    #one has to be added to i and subtracted from i+1
+    y = np.where(np.diff(v))[0]
+    new_df = month_df.loc[y] #queries the original dataframe to extract only relevent indices where the conditions are met
+    #create another column with the end date
+    #first reset the indices 
+    new_df = new_df.reset_index(drop = True)
+    
+    new_df.insert(1, 'End Date', None)#specifies location for adding the end date column
+    new_df.loc[0:(len(new_df)-2), 'End Date'] = new_df['Start Date'][1:].reset_index(drop = True)
+    new_df['End Date'] = pd.to_datetime(new_df["End Date"])
+    #calculate the difference between each element 
+    #these are hourly calculations
+    new_df.insert(2, 'Duration', (new_df['End Date'] - new_df['Start Date']).astype('timedelta64[h]'))
+    
+    #when binning monthly, there is a cell when the year changes. 
+    #this means that the duration will always be around a year's difference
+    #we first identify where the year changes, then make that cell none
+    #np.where identifies where the year changes
+    #with loc, we alter the cell value
+    new_df.loc[np.where((new_df['End Date'].dt.year - new_df['Start Date'].dt.year) >= 1)[0], 'Duration'] = None
+    
+    for i in results['>= Period (h)']:
+        x = (new_df[new_df['Duration'] >= i])['Duration'].sum(axis = 0)/len(new_df)
+        results.loc[results['>= Period (h)'] == i, col] = x
+    #calculate the percentages for when the values are greater than each 
+    #of the durations specified by the period column of results
 
 
+
+results = round(results, 1).astype(float) #np.round and df.round were rounding off to ceiling
+col_width = [.1] * 13
+col_width.insert(0, 0.2)
+fig, ax = plt.subplots(1,1, figsize=(30,30))
+tab = ax.table(cellText = results.values, colLabels = results.columns, 
+          loc = 'center', cellLoc='center', colWidths = col_width)
+plt.axis('off')
+plt.tight_layout()
+plt.rcParams['axes.titley'] = .65   # y is in axes-relative coordinates.
+ax.set_title('rcParam y')
+ax.set_title('Wind & Wave Persistance (%) \n$H_s$ <= {} m, V <= {}'.format(swh_limit, vel_limit))
+for (row, col), cell in tab.get_celld().items():
+  if (row == 0) or (col == -1):
+    cell.set_text_props(fontproperties=FontProperties(weight='bold'))
+
+plt.savefig('Plots/weather_window_table_H{}_V{}.pdf'.format(swh_limit, vel_limit))
